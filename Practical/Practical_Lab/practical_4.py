607aee6305d572019fb36f49a5d746579d933fff
02240122 <02240122.cst@rub.edu.bt>
2025-03-13 19:58:41 +0600

    14th

--- Practical/Practical_Lab/practical_4.py
+++ Practical/Practical_Lab/practical_4.py
@@ -5,3 +5,23 @@ array_len = len(array)
 for index in range(array_len):
     new_array.append(array.pop().lower())
 print(new_array)
+
+array = ["ECE", "EE", "SWE", "WRE", "EG", "ICE", "A", "IT", "CE", "ME"]
+new_array = []
+array_len = len(array)
+
+index = array_len
+for index in range(array_len):
+    new_array.append(array.pop())
+    index -= 1
+print(new_array)
+
+array = ["ECE", "EE", "SWE", "WRE", "EG", "ICE", "A", "IT", "CE", "ME"]
+new_array = []
+array_len = len(array)
+
+index = array_len - 1
+while index >= 0:
+    new_array.append(array.pop().lower())
+    index -= 1
+print(new_array)
